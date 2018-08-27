d63009aa2f6352f9ff59ebeef10ed340450ccc4b
Andrew Parmenter <aparmenter@deloitte.com>
2018-08-27 13:24:59 -0400

    remove duplicated directory

--- migrations/2_deploy_contracts.js
+++
@@ -1,9 +0,0 @@
-var NewToken = artifacts.require("NewToken");
-var IPFShash = artifacts.require("IPFShash");
-var LaunchERC721 = artifacts.require("LaunchERC721");
-
-module.exports = function(deployer) {
-  deployer.deploy(NewToken);
-  deployer.deploy(IPFShash);
-  deployer.deploy(LaunchERC721, "Best Token", "BT");
-};
