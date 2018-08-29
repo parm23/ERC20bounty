3f6b181d0e97c1b58a61c54b4cd8789753782edb
parm23 <42729062+parm23@users.noreply.github.com>
2018-08-29 09:02:13 -0400

    comments

--- Fproj/contracts/IPFShash.sol
+++ Fproj/contracts/IPFShash.sol
@@ -7,17 +7,23 @@ contract IPFShash is Pausable {
     function kill() private {
         if (msg.sender == owner) selfdestruct(owner);
     }
+    
+    //defining event for setting of the hash
     event HashSet(
     string _message
     );
     bytes32 public ipfsHash;
  
     function sendHash(string x) public {
+    
+    //using keccack256 (SHA3) to hash the user input data
+    //string data must first be must be encode packed in order for keccack operation to be valid
         ipfsHash = keccak256(abi.encodePacked(x));
-
+        
+    //logging that a hash has been sent
         emit HashSet("Hash stored successfully!");
     }
-
+    //getter for hashing 
     function getHash() public view returns (bytes32 x) {
         return ipfsHash;
     }
