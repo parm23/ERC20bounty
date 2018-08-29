a152c342b37af3689339c9c08d41666fdad40dff
parm23 <42729062+parm23@users.noreply.github.com>
2018-08-29 09:08:17 -0400

    Update LaunchERC721.sol

--- Fproj/contracts/LaunchERC721.sol
+++ Fproj/contracts/LaunchERC721.sol
@@ -1,13 +1,18 @@
 pragma solidity ^0.4.23;
 
+//importing openzeppelin library 
 import "openzeppelin-solidity/contracts/token/ERC721/ERC721Token.sol";
 import "openzeppelin-solidity/contracts/lifecycle/Pausable.sol";
 
 contract LaunchERC721 is ERC721Token {
     address owner = msg.sender;
+    
+    //inserting kill switch
     function kill() private {
         if (msg.sender == owner) selfdestruct(owner);
     }
+    
+    //ERC721 token standard
     constructor (string _name, string _symbol) public
         ERC721Token(_name, _symbol)
     {
